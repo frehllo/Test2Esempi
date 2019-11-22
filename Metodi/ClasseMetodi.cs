ded3c41dc052f777e444bd36b8351a43fbbd2742
narcisbuzatu2003@gmail.com <narcisbuzatu2003@gmail.com>
2019-11-22 20:06:43 +0100

    Aggiungere i file di progetto.

---
+++ Metodi/ClasseMetodi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Metodi
+{
+    public class ClasseMetodi
+    {
+        public static bool Esempio1()
+        {
+            double a = 1.0 / 10;
+            double prodotto = a * 10;
+            return true;
+        }
+    }
+}
---
+++ TestMetodi/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestMetodi
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
