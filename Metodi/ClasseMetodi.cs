6fb299f5d77a5361c9d00796e39b4ca6ca13fc29
narcisbuzatu2003@gmail.com <narcisbuzatu2003@gmail.com>
2019-11-22 20:33:14 +0100

    Test 2 esempi

--- Metodi/ClasseMetodi.cs
+++ Metodi/ClasseMetodi.cs
@@ -6,9 +6,21 @@ namespace Metodi
     {
         public static bool Esempio1()
         {
-            double a = 1.0 / 10;
+            double a = 1.0 / 10.0;
             double prodotto = a * 10;
-            return true;
+            if (prodotto == 1.0)
+                return true;
+            else
+                return false;
+        }
+        public static bool Esempio2()
+        {
+            double a = 1.0 / 10.0;
+            double somma = a + a + a + a + a + a + a + a + a + a;
+            if (somma == 1)
+                return true;
+            else
+                return false;
         }
     }
 }
--- TestMetodi/UnitTest1.cs
+++ TestMetodi/UnitTest1.cs
@@ -6,8 +6,17 @@ namespace TestMetodi
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestEsempio1()
         {
+            bool risp = Metodi.ClasseMetodi.Esempio1();
+            Assert.AreEqual(true, risp);
+        }
+        [TestMethod]
+
+        public void TestEsempio2()
+        {
+            bool risp = Metodi.ClasseMetodi.Esempio1();
+            Assert.AreEqual(true, risp);
         }
     }
 }
